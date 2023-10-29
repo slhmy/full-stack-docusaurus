917a9df59315f829acdbd763bac6e6fab818d374
slhmy <1484836413@qq.com>
2023-10-29 11:14:53 +0800

    Fix broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -106,12 +106,7 @@ const config = {
         links: [
           {
             title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
+            items: [],
           },
           {
             title: "Community",
